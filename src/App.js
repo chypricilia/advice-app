ad7766a1c219ce60ed8401800241c91943551184
Pricilia <chypricilianna@gmail.com>
2021-02-02 10:28:30 +0800

    add function to change advice when user click on button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
         };
 
         this.getAdvice = this.getAdvice.bind(this);
+        this.getAdviceByID = this.getAdviceByID.bind(this);
     }
 
     componentDidMount() {
@@ -23,11 +24,27 @@ class App extends React.Component {
         ).then(response => {
             const data = response.data.slip.advice;
             console.log(data);
-
             this.setState({advice : data});
         }).catch(error => {
             console.log(error);
         });
+    }
+
+    getAdviceByID() {
+        const id = parseInt(Math.random()*200);
+
+        axios.get(
+            `https://api.adviceslip.com/advice/${id}`
+        ).then(response => {
+            console.log(id);
+            const data = JSON.parse(response.data + "}");
+            const advice = data.slip.advice;
+            console.log(advice);
+            this.setState({advice : advice});
+        }).catch(error => {
+            console.log(error);
+        });
+        
     }
 
     render(){
@@ -37,7 +54,7 @@ class App extends React.Component {
             <div className="app">
                 <div className="card">
                     <h1 className="header"> {advice}</h1>
-                    <button className ="button">
+                    <button className ="button" onClick={this.getAdviceByID}>
                         <span>GIVE ME ADVICE!</span>
                     </button>
                 </div>
