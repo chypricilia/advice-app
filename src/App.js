2c27a0a75582c79fae469054c4cde9c7d215bc6e
Pricilia <chypricilianna@gmail.com>
2021-02-02 09:02:19 +0800

    fetch data from API and show it on console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+
+
+    componentDidMount() {
+        this.getAdvice();
+    }
+
+    getAdvice () {
+        axios.get(
+            'https://api.adviceslip.com/advice'
+        ).then(response => {
+            const data = response.data.slip.advice;
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
 
     render(){
         return (
