dd3285e742302fe99f759986c1de1967568d5f87
Pricilia <chypricilianna@gmail.com>
2021-02-02 08:47:05 +0800

    install axios into react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-    
+
     render(){
         return (
             <h1> Test My App</h1>
         );
     }
 }
+
+export default App;
