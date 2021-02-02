2778e9b2e2708736002ad79ec5b602909877d9bf
Pricilia <chypricilianna@gmail.com>
2021-02-02 09:18:10 +0800

    display advice on homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 
 class App extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            advice: ''
+        };
+
+        this.getAdvice = this.getAdvice.bind(this);
+    }
 
     componentDidMount() {
         this.getAdvice();
@@ -14,14 +22,18 @@ class App extends React.Component {
         ).then(response => {
             const data = response.data.slip.advice;
             console.log(data);
+
+            this.setState({advice : data});
         }).catch(error => {
             console.log(error);
         });
     }
 
     render(){
+
+        const {advice} = this.state;
         return (
-            <h1> Test My App</h1>
+            <h1> {advice}</h1>
         );
     }
 }
